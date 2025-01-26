c1f2d5d8da131890c3129dbf14b8394be1c97422
praveen18111999 <praveenraj8028@gmail.com>
2025-01-26 22:11:39 +0530

    modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,10 @@ import { SkillsComponent } from './skills/skills.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
+  {path:'header',component:HeaderComponent},
   {path:'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'skills',component:SkillsComponent},
